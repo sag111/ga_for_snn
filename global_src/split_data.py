a9dca29670701a9996962dcd4570ee1358798cc0
aserenko <serenko@phystech.edu>
2020-07-07 20:29:16 +0300

    Refactor split_data and add the Optdigits dataset

--- global_src/split_data.py
+++ global_src/split_data.py
@@ -1,6 +1,6 @@
 import sys  # for path
 from os import chdir, mkdir
-import numpy
+import numpy as np
 
 import sklearn
 import sklearn.model_selection
@@ -10,24 +10,62 @@ import sklearn.datasets
 sys.path.append('.')
 from parameters.parameters_network import network_parameters
 
+def load_mnist(**kwargs):
+    X, y = sklearn.datasets.fetch_openml('mnist_784', **kwargs)
+    y = y.astype(int)
+    return X, y
+
 def load_dataset():
-    if network_parameters['dataset'] == 'Iris':
-        dataset_bunch = sklearn.datasets.load_iris()
-    if network_parameters['dataset'] == 'Digits':
-        dataset_bunch = sklearn.datasets.load_digits()
-    if network_parameters['dataset'] == 'Wisconsin':
-        dataset_bunch = sklearn.datasets.load_breast_cancer()
-    if network_parameters['dataset'] == 'MNIST':
-        dataset_bunch = sklearn.datasets.fetch_openml('mnist_784')
+    if network_parameters['dataset'] == 'Optdigits':
+        import csv
+        X_and_y_train, X_and_y_test = [
+            np.array([
+                line for line in csv.reader(open(
+                    '/s/ls4/users/aserenko/'
+                    'Genetic/Optdigits/original_data/'
+                    'optdigits.' + tra_or_tes
+                ))
+            ]).astype(int)
+            for tra_or_tes in ('tra', 'tes')
+        ]
+        (X_train, y_train), (X_test, y_test) = [
+            [
+                X_and_y[:,:-1],
+                X_and_y[:,-1]
+            ]
+            for X_and_y in (X_and_y_train, X_and_y_test)
+        ]
+        return {
+            'train': (X_train, y_train),
+            'test': (X_test, y_test)
+        }
 
-    X = dataset_bunch['data']
-    y = dataset_bunch['target']
-
+    dataset_loaders = {
+        'Iris': sklearn.datasets.load_iris,
+        'Digits': sklearn.datasets.load_digits,
+        'Wisconsin': sklearn.datasets.load_breast_cancer,
+        'MNIST': load_mnist
+    }
+    load_the_chosen_dataset = dataset_loaders[network_parameters['dataset']]
+    X, y = load_the_chosen_dataset(return_X_y=True)
     # Flatten the images.
     X = X.reshape(
         (X.shape[0], -1)
     )
 
+    if network_parameters['dataset'] == 'MNIST':
+        # MNIST has a precomposed testing set.
+        # So, we train on a random subset (but
+        # not overlapping the testing set),
+        # and test on all the 10 000 last instances.
+        X_train = X[:-10000]
+        y_train = y[:-10000]
+        X_test = X[-10000:]
+        y_test = y[-10000:]
+        return {
+            'train': (X_train, y_train),
+            'test': (X_test, y_test)
+        }
     return X, y
 
 def encode_with_gaussian_receptive_fields(X):
@@ -36,7 +74,7 @@ def encode_with_gaussian_receptive_fields(X):
         cutoff_gaussian = 0.09
         result = (
             # So that the maximum value, if x == mu, is 1
-            numpy.e ** (- (x - mu) ** 2 / (2 * sigma_squared))
+            np.e ** (- (x - mu) ** 2 / (2 * sigma_squared))
         )
         if result < cutoff_gaussian:
             result = 0
@@ -52,7 +90,7 @@ def encode_with_gaussian_receptive_fields(X):
     )**2
 
     result = [
-        numpy.concatenate([
+        np.concatenate([
             [
                 get_gaussian(
                     x=x_component,
@@ -65,21 +103,18 @@ def encode_with_gaussian_receptive_fields(X):
         ])
         for x in X
     ]
-    return numpy.array(result)
+    return np.array(result)
 
 
 if __name__ == "__main__":
-    X, y = load_dataset()
-
-    if network_parameters['dataset'] == 'MNIST':
-        # MNIST has a precomposed testing set.
-        # So, we train on a random subset (but
-        # not overlapping the testing set),
-        # and test on all the 10 000 last instances.
-        test_X = X[-10000:]
-        test_y = y[-10000:]
-        X = X[:-10000]
-        y = y[:-10000]
+    data = load_dataset()
+    if type(data) is tuple:
+        X, y = data
+        testing_set_is_precomposed = False
+    if type(data) is dict:
+        X, y = data['train']
+        X_test_precomposed, y_test_precomposed = data['test']
+        testing_set_is_precomposed = True
 
     splits = sklearn.model_selection.StratifiedKFold(
         n_splits=5,
@@ -93,12 +128,18 @@ if __name__ == "__main__":
     # ).split(X, y)
     for fold, current_split in enumerate(splits):
         train_indices, test_indices = current_split
-        train_X = X[train_indices]
-        train_y = y[train_indices]
-        if network_parameters['dataset'] != 'MNIST':
-            test_X = X[test_indices]
-            test_y = y[test_indices]
-        # Else test_X and test_y are predefined.
+        X_train = X[train_indices]
+        y_train = y[train_indices]
+        if not testing_set_is_precomposed:
+            X_test = X[test_indices]
+            y_test = y[test_indices]
+        else:
+            # Copy to work around multiple normalization.
+            X_test = X_test_precomposed.copy()
+            y_test = y_test_precomposed
+            # we still need bookkeeping of
+            # which neuron gets which indices
+            test_indices = np.array(range(len(y_test)))
 
         def normalize(X):
             for norm_type in network_parameters['normalize'].split('+'):
@@ -107,7 +148,7 @@ if __name__ == "__main__":
                 if norm_type == 'L1':
                     X = sklearn.preprocessing.normalize(X, norm = 'l1', axis = 1)
                 if norm_type == 'complement':
-                    X = numpy.concatenate(
+                    X = np.concatenate(
                         [
                             X,
                             # append a complement to x
@@ -118,15 +159,15 @@ if __name__ == "__main__":
                 if norm_type == 'minmaxscale':
                     X = sklearn.preprocessing.minmax_scale(X, copy=False)
             return X
-        train_X, test_X = [
-            normalize(train_or_test_X)
-            for train_or_test_X in (train_X, test_X)
+        X_train, X_test = [
+            normalize(X_train_or_test)
+            for X_train_or_test in (X_train, X_test)
         ]
 
         if network_parameters['gaussian_encoding'] == True:
-            train_X, test_X = map(
+            X_train, X_test = map(
                 encode_with_gaussian_receptive_fields,
-                (train_X, test_X)
+                (X_train, X_test)
             )
 
         mkdir('fold' + str(fold))
@@ -135,28 +176,34 @@ if __name__ == "__main__":
         mkdir('train_data')
         mkdir('test_data')
 
-        numpy.savetxt('train_data/indices.txt', train_indices, '%d')
-        numpy.savetxt('test_data/indices.txt', test_indices, '%d')
+        np.savetxt('train_data/indices.txt', train_indices, '%d')
+        if not testing_set_is_precomposed:
+            np.savetxt('test_data/indices.txt', test_indices, '%d')
 
         for current_class in set(y):
-            current_train_X = train_X[train_y == current_class]
-            current_test_X = test_X[test_y == current_class]
+            current_X_train = X_train[y_train == current_class]
+            current_X_test = X_test[y_test == current_class]
 
-            # Trim the data to decrease simulation time.
-            max_train_size = 30
-            max_test_size = 10
-            current_train_X = current_train_X[:max_train_size]
-            current_test_X = current_test_X[:max_test_size]
-
-            numpy.savetxt(
+            np.savetxt(
                 'train_data/mask_class' + str(current_class) + '.txt',
-                current_train_X,
+                current_X_train,
                 '%.3f'
             )
-            numpy.savetxt(
+            np.savetxt(
+                'train_data/indices_class' + str(current_class) + '.txt',
+                train_indices[y_train == current_class],
+                '%d'
+            )
+
+            np.savetxt(
                 'test_data/mask_class' + str(current_class) + '.txt',
-                current_test_X,
+                current_X_test,
                 '%.3f'
+            )
+            np.savetxt(
+                'test_data/indices_class' + str(current_class) + '.txt',
+                test_indices[y_test == current_class],
+                '%d'
             )
 
         chdir('..') # out of fold*
